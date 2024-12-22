08ea97a8f1f11e05822bfa1d873ed7b160f51c48
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2024-12-22 16:15:35 +0100

    Fully functioning webserver to serve a file with correct http status

--- server.py
+++ server.py
@@ -9,6 +9,22 @@ print('Listening on port 8000!')
 
 while True:
     client_socket, client_addr = server_socket.accept()
-    print(f'Now communicating with {client_socket}, {client_addr}')
     client_request = client_socket.recv(1024).decode()
-    print(f'The request sent was\n\n{client_request}')
+    client_requests = client_request.split('\n')
+    header = client_requests[0].split(' ') 
+    method = header[0]
+    requested_path = header[1]
+    if method == 'GET':
+        print(f'GET request from {client_addr} for {requested_path}')
+        if requested_path == '/' or '/index.html':
+            response_file = open('./index.html')
+            page = response_file.read()
+            response_file.close()
+            response = 'HTTP/1.1 200 OK\n\n' + page
+
+    else:
+        print(f'Faulty request from {client_addr}')
+        response = 'HTTP/1.1 405 Method Not Allowed\n\nAllowed methods: GET'
+        
+    client_socket.sendall(response.encode())
+    client_socket.close()
